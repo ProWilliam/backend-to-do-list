e7c2487e9ec34fc9bfec4b459a65bb374c3561e8
William Torre <pro.williamtoro@gmail.com>
2024-09-28 10:59:27 -0500

    The application is typed, created, a logger is created for requests to the server from any route, a server connection is created and passed as the main parameter to listen to http and ws requests

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,36 @@
-import express from 'express';
+import express, { Application } from 'express';
+import logger  from 'morgan';
 import cors from 'cors';
 import connectDB from './config/db';
 import authRoutes from './routes/auth/auth';
 import todoRoutes from './routes/todo/todo';
 import dotenv from 'dotenv';
+import { createWebSocket } from './websocket/websocket';
+import { createServer } from 'http';
 
 dotenv.config();
-const app = express();
 
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
 app.use(cors())
-
 app.use(express.json());
+app.use(logger('dev'));
+
+// Connect database
 connectDB();
 
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+// Initialize WebSocket with HTTP server\
+const server = createServer(app);
+createWebSocket(server);
+
+// Start Server
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
